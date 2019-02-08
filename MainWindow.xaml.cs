98eba6b5dd9c0067309ee520393aad1d69e12343
quinn <36234189+316968@users.noreply.github.com>
2019-02-08 13:56:57 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _316968IPO
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            /*decimal length = 0;
+            decimal width = 0;
+            decimal.TryParse(txtLength.Text, out length);
+            decimal.TryParse(txtWidth.Text, out width);
+
+            decimal area = length * width;
+
+            if (area > 0)
+                MessageBox.Show(area.ToString());
+            else if (area <= 0)
+                MessageBox.Show("area cannot be less or equal to 0");*/
+
+            decimal length = 0;
+
+
+            if (Double.TryParse(txtLength.Text, out double result))
+                MessageBox.Show(result.ToString());
+            else
+                MessageBox.Show("Unable to parse {0}.", txtLength.Text);
+        }
+    }
+}
